62e6c73e836a945f1bc8a15a290da9d8a84b22ad
Vignesh <vigneshragu501@gmail.com>
2020-12-21 15:16:39 +0530

    div method added.

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -16,13 +16,19 @@ public class Calc {
 	{
 		return num1*num2;
 	}
-	
+
+	public static int div(int num1, int num2)
+	{
+		return num2/num1;
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ADDITION = " + add(10,20));
 		System.out.println("DIFFERENCE = " + sub(10,20));
 		System.out.println("Product = " + pro(10,20));
+		System.out.println("division = " + div(10,20));
 	}
 
 }
